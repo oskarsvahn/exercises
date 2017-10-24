b7d73238295b956738fa7119b00fa2926e91c67f
Linus Törngren <linus@etnolit.se>
2017-10-24 09:56:17 +0200

    Made the tests for advanced.py a bit clearer.

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -11,3 +11,13 @@ def test_factorial():
     assert sorted(factorial(40)) == [2, 2, 2, 5]
     assert sorted(factorial(693)) == [3, 3, 7, 11]
     assert sorted(factorial(39270)) == [2, 3, 5, 7, 11, 17]
+
+
+@pytest.mark.skip('Not yet implemented.')
+def test_yahtzee_score():
+    pass  # Här skriver du som elev tester till uppgiften.
+
+
+@pytest.mark.skip('Not yet implemented.')
+def test_blackjack_score():
+    pass  # Här skriver du som elev tester till uppgiften.
